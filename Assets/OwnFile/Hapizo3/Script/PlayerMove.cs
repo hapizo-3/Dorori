a66f0ac76af1a33c3df1f9a71638e572427b69af
hapizo3 <necosan2019.program@gmail.com>
2020-02-07 13:10:58 +0900

    FirstPush

---
+++ Assets/OwnFile/Hapizo3/Script/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+	Rigidbody2D rb2d;
+	[SerializeField] float jumpPower = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float axisHor = Input.GetAxis( "Horizontal" );
+		float axisVer = Input.GetAxis( "Vertical" );
+
+		if( ( axisHor != 0 ) || ( axisVer != 0 ) )
+		{
+			Debug.Log( axisHor + "," + axisVer );
+			this.gameObject.transform.position += axisHor * ( this.gameObject.transform.right * 0.1f );
+		}
+
+		if( Input.GetKeyDown( "joystick button 0" ) )
+		{
+			//rb2d = GetComponent<Rigidbody2D>();
+			rb2d.AddForce( Vector2.up * jumpPower );
+		}
+
+		//this.gameObject.transform.position += this.gameObject.transform.right;
+    }
+}
---
+++ Assets/OwnFile/Hapizo3/Script/PlayerMove3D.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove3D : MonoBehaviour
+{
+
+	Rigidbody rb;
+	[SerializeField] float jumpPower = 0;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		if( Input.GetKeyDown( "joystick button 0" ) )
+		{
+			//rb2d = GetComponent<Rigidbody2D>();
+			rb.AddForce( Vector3.up * jumpPower );
+		}
+	}
+
+	void FixedUpdate()
+	{
+		//スティック操作取得
+		float axisHor = Input.GetAxis( "Horizontal" );
+		float axisVer = Input.GetAxis( "Vertical" );
+
+		if( ( axisHor != 0 ) || ( axisVer != 0 ) )
+		{
+			//Debug.Log( axisHor + "," + axisVer );
+			Move( axisHor, axisVer );
+		}
+	}
+
+	//プレイヤーを動かす
+	void Move( float axisHor, float axisVer )
+	{
+		this.gameObject.transform.position += axisHor * ( this.gameObject.transform.right * 0.1f );
+	}
+}
